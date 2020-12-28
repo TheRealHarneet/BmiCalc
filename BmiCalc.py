ae220079533c8cab98818201e80518ffb882d0a6
TheRealHarneet <68132395+TheRealHarneet@users.noreply.github.com>
2020-12-27 22:59:50 -0500

    Update BmiCalc.py

--- BmiCalc.py
+++ BmiCalc.py
@@ -1,9 +1,4 @@
-# LB & CM or KG & CM
-# Under 18.5 they are underweight
-# Over 18.5 but below 25 they have a normal weight
-# Over 25 but below 30 they are slightly overweight
-# Over 30 but below 35 they are obese
-# Above 35 they are clinically obese.
+
 
 print("Welcome to my BMI Calculator!")
 weight = int(input("What is your weight in kg?"))
